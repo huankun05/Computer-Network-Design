c681e3bac50c600218f97e8713f993c500eb87a9
huankun05 <66513091+huankun05@users.noreply.github.com>
2026-06-02 21:42:09 +0800

    feat: 计算机网络知识体系交互式展示系统 v1.0

--- src/frontend/src/router/index.jsx
+++ src/frontend/src/router/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import Layout from '../components/Layout'
 import Home from '../pages/Home'
 import DnsVisual from '../pages/DnsVisual'
@@ -10,7 +10,7 @@ import AiChat from '../pages/AiChat'
 import PacketStructure from '../pages/PacketStructure'
 import FaultSimulation from '../pages/FaultSimulation'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <Layout />,
     children: [
